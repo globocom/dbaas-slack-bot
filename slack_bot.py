4d3d3b72b8863e4d999fdd3f1647175284ac07da
otherpirate <mauro_murari@hotmail.com>
2017-08-12 15:37:37 -0300

    Adding bot id in settings

--- slack_bot.py
+++ slack_bot.py
@@ -1,5 +1,6 @@
+from logging import debug
 from slackclient import SlackClient
-from settings import SLACK_TOKEN, SLACK_PROXIES
+from settings import SLACK_TOKEN, SLACK_PROXIES, SLACK_BOT_ID
 
 
 class Bot(object):
@@ -26,3 +27,71 @@ class Bot(object):
     def send_message(self, message):
         for channel in self.my_channels:
             self.send_message_in_channel(message, channel)
+
+    def receive_command(self):
+        commands = self.slack_client.rtm_read()
+        debug(commands)
+        return commands
+
+    def get_messages(self):
+        commands = self.receive_command()
+        if not commands:
+            return None
+
+        for command in commands:
+            if command['type'] != 'message':
+                continue
+
+            if command.get('bot_id', '') == SLACK_BOT_ID:
+                continue
+
+            yield BotMessage.build(command['channel'], command['text'])
+
+
+class BotMessage(object):
+    HELP_TEXTS = ['help']
+    STATUS_TEXTS = ['status', 'how are you?', 'healthcheck', 'health-check']
+
+    @classmethod
+    def build(cls, channel, text):
+        parsed_text = text.lower()
+        if parsed_text in cls.HELP_TEXTS:
+            return BotMessageHelp(channel, text)
+
+        if parsed_text in cls.STATUS_TEXTS:
+            return BotMessageStatus(channel, text)
+
+        return BotMessageInvalid(channel, text)
+
+    def __init__(self, channel, text):
+        self.channel = channel
+        self.text = text
+
+    @property
+    def message(self):
+        raise NotImplementedError
+
+    def __str__(self):
+        return '{}-{}'.format(self.channel, self.text)
+
+
+class BotMessageHelp(BotMessage):
+
+    @property
+    def message(self):
+        return "Mensagem nova pra validar as cozas"
+
+
+class BotMessageStatus(BotMessage):
+
+    @property
+    def message(self):
+        return "I'm a dbaas-error-bot and it's a status message"
+
+
+class BotMessageInvalid(BotMessageHelp):
+
+    @property
+    def message(self):
+        base_help = super(BotMessageInvalid, self).message
+        return "I do not understand it {}".format(self.text) + base_help
