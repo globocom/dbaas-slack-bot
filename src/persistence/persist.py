81ba8280d08b30e102838c00b870d4c58818aa7b
Elesbom <rafael.goncalves@corp.globo.com>
2017-10-24 13:39:49 -0200

    Use redis_sentinel_url

--- src/persistence/persist.py
+++ src/persistence/persist.py
@@ -1,11 +1,13 @@
-from redis import StrictRedis
+# from redis import StrictRedis
+import redis_sentinel_url
 from src.settings import REDIS_URL_CONNECTION, REDIS_KEY_TTL
 
 
 class Persistence(object):
 
     def __init__(self):
-        self.client = StrictRedis.from_url(REDIS_URL_CONNECTION)
+        # self.client = StrictRedis.from_url(REDIS_URL_CONNECTION)
+        self.sentinel, self.client = redis_sentinel_url.connect('redis+{}'.format(REDIS_URL_CONNECTION))
         self.ttl_seconds = REDIS_KEY_TTL
 
     def was_notified(self, task):
