a5132d48c7b81050d09f79a597b342138c18fb72
lefreire <letfreirefigueiredo@gmail.com>
2018-10-10 10:47:57 -0300

    Fixing command to pep8 in init method

--- src/persistence/persist.py
+++ src/persistence/persist.py
@@ -1,4 +1,4 @@
-from redis.sentinel import StrictRedis, Sentinel
+from redis.sentinel import Sentinel
 from src.settings import (REDIS_KEY_TTL, DBAAS_SENTINEL_ENDPOINT_SIMPLE,
                           DBAAS_SENTINEL_SERVICE_NAME, DBAAS_SENTINEL_PASSWORD)
 
@@ -9,9 +9,9 @@ class Persistence(object):
         hosts_sentinel = DBAAS_SENTINEL_ENDPOINT_SIMPLE.replace("sentinel://", "")
         sentinels = list(map(lambda sentinel: tuple(sentinel.split(':')), hosts_sentinel.split(',')))
         sentinel = Sentinel(sentinels, socket_timeout=1)
-        self.client = sentinel.master_for(DBAAS_SENTINEL_SERVICE_NAME, socket_timeout=1,
-        password=DBAAS_SENTINEL_PASSWORD)
-        self.ttl_seconds = REDIS_KEY_TTL
+        self.client = sentinel.master_for(DBAAS_SENTINEL_SERVICE_NAME,
+                                          socket_timeout=1,
+                                          password=DBAAS_SENTINEL_PASSWORD)
 
     def was_notified(self, task):
         return self.client.exists(task.id)
