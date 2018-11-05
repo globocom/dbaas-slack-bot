ce685b7c814d51eb0bbb7c07ac51c68f4c3b70a2
lefreire <letfreirefigueiredo@gmail.com>
2018-11-05 15:33:54 -0200

    Revert "Merge branch 'master' into new_channel"
    This reverts commit 2de332c5850705914a9d1fe7de74474b1ff4fb6a, reversing
    changes made to 59a4966ee9f3b925d283c13039c35c8e4b8583b7.

--- src/persistence/persist.py
+++ src/persistence/persist.py
@@ -1,4 +1,3 @@
-# from redis import StrictRedis
 from redis.sentinel import Sentinel
 from src.settings import (REDIS_KEY_TTL, DBAAS_SENTINEL_ENDPOINT_SIMPLE,
                           DBAAS_SENTINEL_SERVICE_NAME, DBAAS_SENTINEL_PASSWORD)
@@ -10,7 +9,11 @@ class Persistence(object):
         hosts_sentinel = DBAAS_SENTINEL_ENDPOINT_SIMPLE.replace("sentinel://", "")
         sentinels = list(map(lambda sentinel: tuple(sentinel.split(':')), hosts_sentinel.split(',')))
         sentinel = Sentinel(sentinels, socket_timeout=1)
-        self.client = sentinel.master_for(DBAAS_SENTINEL_SERVICE_NAME, socket_timeout=1, password=DBAAS_SENTINEL_PASSWORD)
+        self.client = sentinel.master_for(
+            DBAAS_SENTINEL_SERVICE_NAME,
+            socket_timeout=1,
+            password=DBAAS_SENTINEL_PASSWORD
+        )
         self.ttl_seconds = REDIS_KEY_TTL
 
     def was_notified(self, task):
@@ -29,4 +32,4 @@ class Persistence(object):
         relevance_query = "{}_*".format(relevance)
         keys_list = self.client.keys(relevance_query)
         return [self.client.get(key) for key in keys_list]
-      
+
